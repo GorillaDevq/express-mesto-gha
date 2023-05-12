8fa250e04835060349c2f26273ae088339811b47
Gorilla Dev <gorilladevq@gmail.com>
2023-05-12 21:43:23 +0300

    fetch: logs

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 // eslint-disable-next-line
 const regex = /^(http|https):\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,}(\/\S*)?$/;
 const NotFoundError = require('./errors/NotFoundError');
@@ -16,10 +16,14 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.use(requestLogger);
+
 app.use('/', require('./routes/user'));
 app.use('/', require('./routes/card'));
 
 app.use('*', (req, res, next) => next(new NotFoundError('Ошибка 404')));
+
+app.use(errorLogger);
 
 app.use(errors());
 
